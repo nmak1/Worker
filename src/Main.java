3f5daef28f1d2440d56cc50dc8f10b2a02b42e85
nmak1 <nmak1@rambler.ru>
2022-05-19 20:49:29 +0300

    add errorCallback

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Worker.OnTaskDoneListener listener = System.out::println;
-        Worker worker = new Worker(listener);
+        Worker.OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener,errorListener);
         worker.start();
     }
 
--- src/Worker.java
+++ src/Worker.java
@@ -5,14 +5,29 @@ public class Worker {
         void onDone(String result);
     }
 
-    private OnTaskDoneListener callback;
-
-    public Worker(OnTaskDoneListener callback) {
-        this.callback = callback;
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
     }
+
+    final OnTaskErrorListener errorCallback;
+
+
+    final OnTaskDoneListener callback;
+
+    public Worker(OnTaskDoneListener callback,OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
     public void start() {
         for (int i = 0; i < 100; i++) {
-            callback.onDone("Task " + i + " is done");
+            if (i!=33) {
+                callback.onDone("Task " + i + " is done");
+            } else   errorCallback.onError("Task " + i + " is error");
+
+
+
         }
     }
 }
